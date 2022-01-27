88c419087ddc707e1414428bf74e991d45fa0b35
Administrator <Administrator@PROJECTL>
2022-01-27 11:09:59 +0200

    add ameal class

---
+++ src/CreationalPatterns/AbstractFactory/MealSimpleFactory/Meals/ameal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MealSimpleFactory.Meals.Common;
+
+namespace MealSimpleFactory.Meals
+{
+    public class ameal : IMeal
+    {
+        public void ShowAmountOfCalories()
+        {
+            Console.Write("Calories: 2");
+        }
+        public void ShowDescription()
+        {
+            Console.Write("nice meal");
+        }
+    }
+}
